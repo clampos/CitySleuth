81e27ccebf096f08eed58c9a484e7a7fe1937c08
oclamp01 <oclamp01@student.bbk.ac.uk>
2023-07-22 21:40:40 +0100

    Added marker to embedded Google Maps image and work begun for Places API operations

--- public/mapScript.js
+++ public/mapScript.js
@@ -1,20 +1,26 @@
 function initMap() {
-  myMap = new google.maps.Map(document.getElementById("map"));
-
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      // Center on user's current location if geolocation prompt allowed
-      var initialLocation = new google.maps.LatLng(
+      var latLng = new google.maps.LatLng(
         position.coords.latitude,
         position.coords.longitude
       );
-      myMap.setCenter(initialLocation);
-      myMap.setZoom(12);
+      var mapOptions = {
+        zoom: 10,
+        center: latLng,
+      };
+
+      myMap = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+      var marker = new google.maps.Marker({
+        position: latLng,
+      });
+
+      marker.setMap(myMap);
     },
     function (positionError) {
-      // User denied geolocation prompt - default to Charing Cross
       myMap.setCenter(new google.maps.LatLng(51.507, 0.1232));
-      myMap.setZoom(12);
+      myMap.setZoom(10);
     }
   );
 }
