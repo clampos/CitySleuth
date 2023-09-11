db85f755d45c578d78057ba7064a2ca0194dd9f6
oclamp01 <oclamp01@student.bbk.ac.uk>
2023-09-11 20:29:27 +0100

    Updated app.yaml file for GAE deployment

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,30 +1,41 @@
-const app = require("../app.js");
 const supertest = require("supertest");
 const mongoose = require("mongoose");
+const app = require("../app");
 const request = supertest(app);
-require("dotenv").config();
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const { redirect } = require("express/lib/response");
 
 describe("POST /register", () => {
   describe("Given a valid username, valid e-mail address and valid password", () => {
     test("Should return a 302 status", async () => {
       const response = await request.post("/register").send({
-        username: "jestuser30",
-        email: "jestuser30@gmail.com",
-        password: "jestuser12",
+        username: "cassiopeia2",
+        email: "cassiopeia2@gmail.com",
+        password: "cassiopeia",
       });
       expect(response.statusCode).toBe(302);
-      expect("Location").toBe("/login");
     });
   });
 
   describe("Given an invalid username, valid e-mail address and valid password", () => {
     test("Should return a 400 status", async () => {
       const response = await request.post("/register").send({
-        username: "jest",
+        username: "jest", // Fewer than 6 characters, so invalid
         email: "jestuser14@gmail.com",
         password: "jestuser14",
       });
       expect(response.statusCode).toBe(400);
     });
+  });
+});
+
+describe("Given a valid username, valid e-mail address and invalid password", () => {
+  test("SHould return a 400 status", async () => {
+    const response = (await request.post("/register")).send({
+      username: jestuser20,
+      email: "jestuser20@gmail.com",
+      password: "jest", // Fewer than 6 characters, so invalid
+    });
+    expect(response.send).toBe("");
   });
 });
